33ca3288937d464094d0f37eed6506466b88ec9b
DiamondDagger590 <diamonddagger590@gmail.com>
2020-06-21 16:00:00 -0500

    Initial commit

---
+++ src/main/java/us/eunoians/prisma/Prisma.java
@@ -0,0 +1,17 @@
+package us.eunoians.prisma;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Prisma extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
