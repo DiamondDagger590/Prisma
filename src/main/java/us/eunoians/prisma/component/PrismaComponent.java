8d31b2140c27d5d0597b61309b8e39dc53404867
Gijs de Jong <14833076+OxKitsune@users.noreply.github.com>
2020-06-22 00:20:03 +0200

    Fix spigot dependency in build.gradle and delombokify project

--- src/main/java/us/eunoians/prisma/component/PrismaComponent.java
+++ src/main/java/us/eunoians/prisma/component/PrismaComponent.java
@@ -1,9 +1,5 @@
 package us.eunoians.prisma.component;
 
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.ClickEvent;
@@ -18,191 +14,223 @@ import java.util.regex.Pattern;
 /**
  * This code is a modified version of TextComponent from Bungee, with the intent of supporting custom color codes
  */
-@Getter
-@Setter
-@AllArgsConstructor
-@EqualsAndHashCode(callSuper = true)
-public class PrismaComponent extends BaseComponent{
-  
-  private static final Pattern url = Pattern.compile("^(?:(https?)://)?([-\\w_\\.]{2,}\\.[a-z]{2,4})(/\\S*)?$");
-  
-  /**
-   * Converts the old formatting system that used
-   * {@link net.md_5.bungee.api.ChatColor#COLOR_CHAR} into the new json based
-   * system.
-   *
-   * @param message the text to convert
-   * @return the components needed to print the message to the client
-   */
-  public static BaseComponent[] fromLegacyText(String message){
-    return fromLegacyText(message, ChatColor.WHITE);
-  }
-  
-  /**
-   * Converts the old formatting system that used
-   * {@link net.md_5.bungee.api.ChatColor#COLOR_CHAR} into the new json based
-   * system.
-   *
-   * @param message      the text to convert
-   * @param defaultColor color to use when no formatting is to be applied
-   *                     (i.e. after ChatColor.RESET).
-   * @return the components needed to print the message to the client
-   */
-  public static BaseComponent[] fromLegacyText(String message, ChatColor defaultColor){
-    ArrayList<BaseComponent> components = new ArrayList<BaseComponent>();
-    StringBuilder builder = new StringBuilder();
-    TextComponent component = new TextComponent();
-    Matcher matcher = url.matcher(message);
-    
-    for(int i = 0; i < message.length(); i++){
-      char c = message.charAt(i);
-      if(c == ChatColor.COLOR_CHAR){
-        if(++i >= message.length()){
-          break;
+public class PrismaComponent extends BaseComponent {
+
+    private static final Pattern url = Pattern.compile("^(?:(https?)://)?([-\\w_\\.]{2,}\\.[a-z]{2,4})(/\\S*)?$");
+
+    public PrismaComponent(String text) {
+        this.text = text;
+    }
+
+    /**
+     * Converts the old formatting system that used
+     * {@link net.md_5.bungee.api.ChatColor#COLOR_CHAR} into the new json based
+     * system.
+     *
+     * @param message the text to convert
+     * @return the components needed to print the message to the client
+     */
+    public static BaseComponent[] fromLegacyText(String message) {
+        return fromLegacyText(message, ChatColor.WHITE);
+    }
+
+    /**
+     * Converts the old formatting system that used
+     * {@link net.md_5.bungee.api.ChatColor#COLOR_CHAR} into the new json based
+     * system.
+     *
+     * @param message      the text to convert
+     * @param defaultColor color to use when no formatting is to be applied
+     *                     (i.e. after ChatColor.RESET).
+     * @return the components needed to print the message to the client
+     */
+    public static BaseComponent[] fromLegacyText(String message, ChatColor defaultColor) {
+        ArrayList<BaseComponent> components = new ArrayList<BaseComponent>();
+        StringBuilder builder = new StringBuilder();
+        TextComponent component = new TextComponent();
+        Matcher matcher = url.matcher(message);
+
+        for (int i = 0; i < message.length(); i++) {
+            char c = message.charAt(i);
+            if (c == ChatColor.COLOR_CHAR) {
+                if (++i >= message.length()) {
+                    break;
+                }
+                c = message.charAt(i);
+                if (c >= 'A' && c <= 'Z') {
+                    c += 32;
+                }
+                ChatColor format = ChatColor.getByChar(c);
+                if (format == null) {
+                    continue;
+                }
+                if (builder.length() > 0) {
+                    TextComponent old = component;
+                    component = new TextComponent(old);
+                    old.setText(builder.toString());
+                    builder = new StringBuilder();
+                    components.add(old);
+                }
+                switch (format) {
+                    case BOLD:
+                        component.setBold(true);
+                        break;
+                    case ITALIC:
+                        component.setItalic(true);
+                        break;
+                    case UNDERLINE:
+                        component.setUnderlined(true);
+                        break;
+                    case STRIKETHROUGH:
+                        component.setStrikethrough(true);
+                        break;
+                    case MAGIC:
+                        component.setObfuscated(true);
+                        break;
+                    case RESET:
+                        format = defaultColor;
+                    default:
+                        component = new TextComponent();
+                        component.setColor(format);
+                        break;
+                }
+                continue;
+            }
+            int pos = message.indexOf(' ', i);
+            if (pos == -1) {
+                pos = message.length();
+            }
+            if (matcher.region(i, pos).find()) { //Web link handling
+
+                if (builder.length() > 0) {
+                    TextComponent old = component;
+                    component = new TextComponent(old);
+                    old.setText(builder.toString());
+                    builder = new StringBuilder();
+                    components.add(old);
+                }
+
+                TextComponent old = component;
+                component = new TextComponent(old);
+                String urlString = message.substring(i, pos);
+                component.setText(urlString);
+                component.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL,
+                        urlString.startsWith("http") ? urlString : "http://" + urlString));
+                components.add(component);
+                i += pos - i - 1;
+                component = old;
+                continue;
+            }
+            builder.append(c);
         }
-        c = message.charAt(i);
-        if(c >= 'A' && c <= 'Z'){
-          c += 32;
-        }
-        ChatColor format = ChatColor.getByChar(c);
-        if(format == null){
-          continue;
-        }
-        if(builder.length() > 0){
-          TextComponent old = component;
-          component = new TextComponent(old);
-          old.setText(builder.toString());
-          builder = new StringBuilder();
-          components.add(old);
-        }
-        switch(format){
-          case BOLD:
-            component.setBold(true);
-            break;
-          case ITALIC:
-            component.setItalic(true);
-            break;
-          case UNDERLINE:
-            component.setUnderlined(true);
-            break;
-          case STRIKETHROUGH:
-            component.setStrikethrough(true);
-            break;
-          case MAGIC:
-            component.setObfuscated(true);
-            break;
-          case RESET:
-            format = defaultColor;
-          default:
-            component = new TextComponent();
-            component.setColor(format);
-            break;
-        }
-        continue;
-      }
-      int pos = message.indexOf(' ', i);
-      if(pos == -1){
-        pos = message.length();
-      }
-      if(matcher.region(i, pos).find()){ //Web link handling
-        
-        if(builder.length() > 0){
-          TextComponent old = component;
-          component = new TextComponent(old);
-          old.setText(builder.toString());
-          builder = new StringBuilder();
-          components.add(old);
-        }
-        
-        TextComponent old = component;
-        component = new TextComponent(old);
-        String urlString = message.substring(i, pos);
-        component.setText(urlString);
-        component.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL,
-          urlString.startsWith("http") ? urlString : "http://" + urlString));
+
+        component.setText(builder.toString());
         components.add(component);
-        i += pos - i - 1;
-        component = old;
-        continue;
-      }
-      builder.append(c);
+
+        return components.toArray(new BaseComponent[components.size()]);
     }
-    
-    component.setText(builder.toString());
-    components.add(component);
-    
-    return components.toArray(new BaseComponent[components.size()]);
-  }
-  
-  /**
-   * The text of the component that will be displayed to the client
-   */
-  private String text;
-  
-  /**
-   * Creates a TextComponent with blank text.
-   */
-  public PrismaComponent(){
-    this.text = "";
-  }
-  
-  /**
-   * Creates a TextComponent with formatting and text from the passed
-   * component
-   *
-   * @param textComponent the component to copy from
-   */
-  public PrismaComponent(TextComponent textComponent){
-    copyFormatting(textComponent, ComponentBuilder.FormatRetention.ALL, true);
-    
-    if(textComponent.getExtra() != null){
-      for(BaseComponent extra : textComponent.getExtra()){
-        addExtra(extra.duplicate());
-      }
+
+    /**
+     * The text of the component that will be displayed to the client
+     */
+    private String text;
+
+    /**
+     * Creates a TextComponent with blank text.
+     */
+    public PrismaComponent() {
+        this.text = "";
     }
-    setText(textComponent.getText());
-  }
-  
-  /**
-   * Creates a TextComponent with blank text and the extras set to the passed
-   * array
-   *
-   * @param extras the extras to set
-   */
-  public PrismaComponent(BaseComponent... extras){
-    this();
-    if(extras.length == 0){
-      return;
+
+    /**
+     * Creates a TextComponent with formatting and text from the passed
+     * component
+     *
+     * @param textComponent the component to copy from
+     */
+    public PrismaComponent(TextComponent textComponent) {
+        copyFormatting(textComponent, ComponentBuilder.FormatRetention.ALL, true);
+
+        if (textComponent.getExtra() != null) {
+            for (BaseComponent extra : textComponent.getExtra()) {
+                addExtra(extra.duplicate());
+            }
+        }
+        setText(textComponent.getText());
     }
-    setExtra(new ArrayList<>(Arrays.asList(extras)));
-  }
-  
-  /**
-   * Creates a duplicate of this TextComponent.
-   *
-   * @return the duplicate of this TextComponent.
-   */
-  @Override
-  public TextComponent duplicate(){
-    return new TextComponent(this);
-  }
-  
-  public void toPlainText(StringBuilder builder){
-    builder.append(text);
-    toPlainTextCopied(builder);
-  }
-  
-  void toPlainTextCopied(StringBuilder builder){
-    if(getExtra() != null){
-      for(BaseComponent e : getExtra()){
-        //This requires the package private method toPlainText in base component... Idk if we can use reflection or something to expose this or if that's a viable option?
-        e.toPlainText(builder);
-      }
+
+    /**
+     * Creates a TextComponent with blank text and the extras set to the passed
+     * array
+     *
+     * @param extras the extras to set
+     */
+    public PrismaComponent(BaseComponent... extras) {
+        this();
+        if (extras.length == 0) {
+            return;
+        }
+        setExtra(new ArrayList<>(Arrays.asList(extras)));
     }
-  }
-  
-  @Override
-  public String toString(){
-    return String.format("TextComponent{text=%s, %s}", text, super.toString());
-  }
+
+    /**
+     * Creates a duplicate of this TextComponent.
+     *
+     * @return the duplicate of this TextComponent.
+     */
+    @Override
+    public TextComponent duplicate() {
+        return new TextComponent(this);
+    }
+
+    public void toPlainText(StringBuilder builder) {
+        builder.append(text);
+        toPlainTextCopied(builder);
+    }
+
+    void toPlainTextCopied(StringBuilder builder) {
+        if (getExtra() != null) {
+            for (BaseComponent e : getExtra()) {
+                //This requires the package private method toPlainText in base component... Idk if we can use reflection or something to expose this or if that's a viable option?
+                e.toPlainText(builder);
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("TextComponent{text=%s, %s}", text, super.toString());
+    }
+
+    public String getText() {
+        return this.text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof PrismaComponent)) return false;
+        final PrismaComponent other = (PrismaComponent) o;
+        if (!other.canEqual((Object) this)) return false;
+        if (!super.equals(o)) return false;
+        final Object this$text = this.getText();
+        final Object other$text = other.getText();
+        if (this$text == null ? other$text != null : !this$text.equals(other$text)) return false;
+        return true;
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof PrismaComponent;
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = super.hashCode();
+        final Object $text = this.getText();
+        result = result * PRIME + ($text == null ? 43 : $text.hashCode());
+        return result;
+    }
 }
