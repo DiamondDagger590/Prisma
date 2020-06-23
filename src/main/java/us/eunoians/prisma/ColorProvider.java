e3176b7bc0e5495511dc63a1234a72c85b819bd4
DiamondDagger590 <diamonddagger590@gmail.com>
2020-06-23 10:50:56 -0500

    Add creation of PrismaComponent from text

--- src/main/java/us/eunoians/prisma/ColorProvider.java
+++ src/main/java/us/eunoians/prisma/ColorProvider.java
@@ -1,5 +1,10 @@
 package us.eunoians.prisma;
 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,7 +13,33 @@ public class ColorProvider{
   private static Map<Character, RGBWrapper> colorMap = new HashMap<>();
   
   ColorProvider(Prisma prisma){
-  
+    
+    //Get the colors.yml and load it in
+    File file = new File(prisma.getDataFolder(), "colors.yml");
+    FileConfiguration fileConfiguration = YamlConfiguration.loadConfiguration(file);
+    
+    //Iterate through all colors in the colors.yml
+    for(String color : fileConfiguration.getConfigurationSection("ChatColors").getKeys(false)){
+      
+      //Setup our key for extracting values
+      String key = "ChatColors." + color + ".";
+      
+      //Get the chat color char
+      char chatChar = fileConfiguration.getString(key + "ColorCode").charAt(0);
+      
+      //Setup our RGBWrapper
+      RGBWrapper rgbWrapper = null;
+      
+      //Try to make a wrapper from RGB color format
+      if(fileConfiguration.contains(key + "RGB")){
+        String[] RGB = fileConfiguration.getString(key + "RGB").split(":");
+        int red = Integer.parseInt(RGB[0]);
+        int green = Integer.parseInt(RGB[1]);
+        int blue = Integer.parseInt(RGB[2]);
+        rgbWrapper = new RGBWrapper(red, green, blue);
+      }
+      colorMap.put(chatChar, rgbWrapper);
+    }
   }
   
   /**
